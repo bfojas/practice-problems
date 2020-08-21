93fdb39a350f2853a134c00cf7fe68462c726a50
Bradley Fojas <43628838+bfojas@users.noreply.github.com>
2020-08-21 16:16:39 -0700

    longest parentheses

--- javascript-leetcode.js
+++ javascript-leetcode.js
@@ -401,3 +401,41 @@ var reverseVowels = function(s) {
   }
   return arr.join('')
 };
+
+
+
+// Longest Valid Parentheses
+// Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+
+// Example 1:
+
+// Input: "(()"
+// Output: 2
+// Explanation: The longest valid parentheses substring is "()"
+// Example 2:
+
+// Input: ")()())"
+// Output: 4
+// Explanation: The longest valid parentheses substring is "()()"
+
+var longestValidParentheses = function(s) {
+  let ans = 0;
+  let length = 0;
+  let stack = [-1];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      stack.push(i)
+    } else {
+      stack.pop()
+      length = i - stack[stack.length -1]
+      if (length > ans) {
+        ans = length
+      }
+      if (stack.length === 0) {
+        length = 0;
+        stack.push(i)
+      }
+    }
+  }
+  return ans 
+};
